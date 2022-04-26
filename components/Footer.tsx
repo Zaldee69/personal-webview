0ba96258286667703f2c6877e30f925c6cbf91c0
Rizaldy <rizaldy@tilaka.id>
2022-04-27 05:11:27 +0700

    add liveness guide page

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className="flex absolute right-0 left-0 justify-center">
+      <Image src="/images/logo.svg" width={100} height={100} />
+    </div>
+  );
+};
+
+export default Footer;
