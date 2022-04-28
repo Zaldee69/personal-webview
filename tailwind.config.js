bb0a1f6d674bfbf2dabdc2890f7f0640aacd7b81
Rizaldy <rizaldy@tilaka.id>
2022-04-28 16:03:27 +0700

    Create a page to fill in Tilaka Name, Password, and Confirm Password

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily : {
-        "poppins" : [ 'Poppins', 'sans-serif']
-      }
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-    colors : {
-      primary : "#0052CC",
-      white : "#fff",
-      neutral : "rgba(107, 119, 140, 1)"
-    }
+    colors: {
+      primary: "#0052CC",
+      white: "#fff",
+      neutral: "rgba(107, 119, 140, 1)",
+      borderColor: "rgba(223, 225, 230, 1)",
+      label: "rgba(107, 119, 140, 1)",
+      placeholder: "rgba(193, 199, 208, 1)",
+      error: "rgba(255, 86, 48, 1)",
+      black : "#000"
+    },
   },
   plugins: [],
-}
+};
+
