cda00452696e8b67d0454e57636b55bdc5fcec23
Muhammad Adam <muhammad.adam@tilaka.id>
2022-06-13 13:51:38 +0700

    move global script from _document page to _app page and use <Script> Component instead of <script> tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+/**
+ * Use <Script> Component instead of <script> tag.
+ * Unfortunately you can't use the next/script component inside the _document.js page,
+ * for global script use it inside _app.js page instead.
+ * see:
+ * - https://nextjs.org/docs/messages/no-sync-scripts
+ * - https://nextjs.org/docs/messages/no-script-in-document-page#why-this-error-occurred
+ */
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -17,7 +25,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Aguafina+Script&family=Allan&family=Architects+Daughter&family=Berkshire+Swash&family=Give+You+Glory&family=Miss+Fajardose&family=Poppins:wght@200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.3.200/pdf.js"></script>
         </Head>
         <body>
           <Main />
