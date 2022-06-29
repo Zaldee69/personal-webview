b7cb06b3c1c33f84ef4d13fafcfea6927a3fb822
Angga Radifan Sumarna <angga.sumarna0@tilaka.id>
2022-06-29 02:12:44 +0000

    TDP-1948 - Memanggil API /kyc/verification untuk memeriksa liveness

--- redux/app/store.ts
+++ redux/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "../slices/loginSlice";
 import documentSlice from "../slices/documentSlice";
+import livenessSlice from "../slices/livenessSlice";
 export const store = configureStore({
   reducer: {
     login : loginSlice,
     document : documentSlice,
+    liveness: livenessSlice,
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
