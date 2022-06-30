f33a34162ed6bf93aa3f23a00442379a3d03380c
Rizaldy <rizaldy@tilaka.id>
2022-06-30 10:09:51 +0700

    resolve conflict

--- redux/app/store.ts
+++ redux/app/store.ts
@@ -8,11 +8,8 @@ export const store = configureStore({
   reducer: {
     login : loginSlice,
     document : documentSlice,
-<<<<<<< HEAD
     certificate: certificateSlice,
-=======
     signature : signatureSlice,
->>>>>>> 52ac1e7411f2b24baa1f7bfc250bb7d5514760f5
     liveness: livenessSlice,
   },
   middleware: (getDefaultMiddleware) =>
