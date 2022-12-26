f5cc384221820d145188c71296d02ce7bd9ad837
Muhammad Rizaldy <rizaldy@tilaka.id>
2022-12-26 04:07:09 +0000

    [TDP-3195] - Menampilkan pop up ketika disconnect

--- components/atoms/DisconnectModal.tsx
+++ components/atoms/DisconnectModal.tsx
@@ -1,34 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import i18n from "i18";
-import Image from "next/image";
 import { assetPrefix } from "next.config";
 import useNavigatorOnline from "hooks/useNavigatorOnline";
 
 const DisconnectModal = () => {
   const { t }: any = i18n;
-  const status = useNavigatorOnline()
-  const [show, setShow] = useState(true)
+  const status = useNavigatorOnline();
+  const [show, setShow] = useState(true);
 
   return (
     <div
       style={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
-      className={`fixed z-50 items-start transition-all duration-1000 pb-3 justify-center w-full left-0 top-0 h-full ${!status && show ? "flex" : "hidden"}`}
+      className={`fixed z-50 items-center transition-all duration-1000 pb-3 justify-center w-full left-0 top-0 h-full ${
+        !status && show ? "flex" : "hidden"
+      }`}
     >
-      <div className="bg-white max-w-md font-poppins mt-20 pt-5 p-2 pb-3 rounded-md w-full mx-5">
-        <div className="px-5" >
-        <div className="flex items-center gap-5" >
-            <img src={`${assetPrefix}/images/danger.jpg`} alt="danger" height={30} width={30} />
+      <div className="bg-white max-w-md font-poppins pt-5 p-2 pb-3 rounded-md w-full mx-5 ">
+        <div className="px-5 py-5 flex justify-start gap-5 items-start">
+          <img
+            src={`${assetPrefix}/images/danger.jpg`}
+            alt="danger"
+            style={{ height: "1.3rem", width: "2rem" }}
+          />
+          <div>
             <p>{t("offlineModal.title")}</p>
+            <p className="mt-5 text-sm text-[#505F79]">
+              {t("offlineModal.subtitle")}
+            </p>
+          </div>
         </div>
-        <p className="mt-5 text-sm text-[#505F79]" >{t("offlineModal.subtitle")}</p>
-        <div className="flex justify-end" >
-        <button
+        <div className="flex justify-end mr-10">
+          <button
             onClick={() => setShow(false)}
-            className="bg-primary btn my-10 disabled:bg-[#DAE6F8] disabled:text-[#6B778C]/30 hover:opacity-70 text-white font-poppins rounded py-2.5 px-6 font-semibold"
+            className="bg-primary btn my-5 disabled:bg-[#DAE6F8] disabled:text-[#6B778C]/30 hover:opacity-70 text-white font-poppins rounded py-2 px-5 font-semibold"
           >
             {t("close")}
           </button>
-        </div>
         </div>
       </div>
     </div>
