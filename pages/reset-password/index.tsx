89ac8c6cf692357fa49b60645a80d3a4adcb2752
adamcanray <adamcool180@gmail.com>
2023-03-27 16:20:12 +0700

    [TDP-3539] fix ukuran halaman reset-password

--- pages/reset-password/index.tsx
+++ pages/reset-password/index.tsx
@@ -149,7 +149,7 @@ const LinkAccount = (props: Props) => {
         <title>Pengaturan Ulang Kata Sandi</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="px-5 py-9 min-h-screen flex flex-col justify-between">
+      <div className="px-5 py-9 max-w-md mx-auto min-h-screen flex flex-col justify-between">
         <div>
           <p className="font-poppins text-lg font-normal text-neutral800 text-center">
             Pengaturan Ulang Kata Sandi
