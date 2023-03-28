c45865a10e335801413d06eb761e2eb396c1ff4e
Muhammad Adam Canrayneldy <muhammad.adam@tilaka.id>
2023-03-28 03:27:07 +0000

    [TDP-3539] [Integrasi] Integrasi halaman reset password

--- pages/reset-password/success.tsx
+++ pages/reset-password/success.tsx
@@ -56,12 +56,12 @@ const LinkAccount = (props: Props) => {
         <title>Pengaturan Ulang Kata Sandi Berhasil</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="px-5 py-9 min-h-screen flex flex-col justify-center">
+      <div className="px-5 py-9 max-w-md mx-auto min-h-screen flex flex-col justify-center">
         <div className="text-center">
           <p className="font-poppins text-lg font-normal text-neutral800 text-center">
             Pengaturan Ulang Kata Sandi Berhasil
           </p>
-          <div className="mt-9">
+          <div className="mt-12">
             <Image
               src={`${assetPrefix}/images/linkAccountSuccess.svg`}
               width="151px"
@@ -69,6 +69,9 @@ const LinkAccount = (props: Props) => {
               alt="liveness-success-ill"
             />
           </div>
+          <p className="mt-4 font-poppins text-sm font-normal text-neutral800 text-center">
+            Silahkan login kembali menggunakan Kata Sandi Anda yang terbaru
+          </p>
           {autoRedirect && redirect_url && (
             <p className="mt-4 font-poppins text-sm font-normal text-neutral800 text-center">
               Silahkan login ulang dalam {currentSecond} detik
