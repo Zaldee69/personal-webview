209d37bad445071dcb9e9c6bb4776fa7488b4dae
adamcanray <adamcool180@gmail.com>
2023-03-28 09:47:44 +0700

    [TDP-3539] fix tampilan

--- pages/reset-password/success.tsx
+++ pages/reset-password/success.tsx
@@ -56,7 +56,7 @@ const LinkAccount = (props: Props) => {
         <title>Pengaturan Ulang Kata Sandi Berhasil</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="px-5 py-9 min-h-screen flex flex-col justify-between">
+      <div className="px-5 py-9 min-h-screen flex flex-col justify-center">
         <div className="text-center">
           <p className="font-poppins text-lg font-normal text-neutral800 text-center">
             Pengaturan Ulang Kata Sandi Berhasil
@@ -75,7 +75,7 @@ const LinkAccount = (props: Props) => {
             </p>
           )}
         </div>
-        <div className="mt-48 flex justify-center">
+        <div className="mt-32 flex justify-center">
           <Image
             src={`${assetPrefix}/images/poweredByTilaka.svg`}
             alt="powered-by-tilaka"
