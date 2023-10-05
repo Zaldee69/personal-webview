8bc455f03a750ba532d2a24d865432f6c04e5b76
Muhammad Rizaldy <rizaldy@tilaka.id>
2023-10-05 10:48:06 +0000

    [TDP-4307] - Standarisasi balikan dan parameter balikan redirect pada WebviewTdp 4178

--- pages/signing/authpin.tsx
+++ pages/signing/authpin.tsx
@@ -66,7 +66,7 @@ const AuthPinForm = (props: Props) => {
             const params = {
               user_identifier: user,
               status: "Sukses",
-              request_id: router.query.request_id || router.query.register_id
+              request_id: res.request_id
             };
             const queryString = new URLSearchParams(params as any).toString();
             window.top!.location.href = concateRedirectUrlParams(
